465c7eec3166986b95704caaac2d4603aea9a67e
Kanav Gupta <33966400+kanav99@users.noreply.github.com>
2020-02-25 02:04:01 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bruter_sdslabs",
-    version="0.0.1",
+    version="0.0.2",
     author="SDSLabs",
     author_email="contact@sdslabs.co.in",
     description="String bruteforcer",
