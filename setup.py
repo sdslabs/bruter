d47d47ff65efdfb215df5474ceba2b9b3f4f5e62
Kanav Gupta <33966400+kanav99@users.noreply.github.com>
2020-02-25 02:10:00 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="bruter_sdslabs",
+    name="bruter",
     version="0.0.2",
     author="SDSLabs",
     author_email="contact@sdslabs.co.in",
